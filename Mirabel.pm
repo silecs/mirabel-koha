dc9971bf2b1b6dc8967ad43e625b80abe6a80dd8
François Gannaz <francois.gannaz@silecs.info>
2014-03-27 15:02:48 +0100

    ignore empty lines in data config

--- Mirabel.pm
+++ Mirabel.pm
@@ -70,7 +70,7 @@ sub read_data_config {
     init();
     my $properfile = $path . "properdata.txt";
     open my $pdfh, "<", $properfile or die "$properfile : $!";
-    my $properdata = { map { chomp; my ($key,$value) = split /;/,$_; ( $key => $value ); } <$pdfh> };
+    my $properdata = { map { chomp; next if /^\s*$/; my ($key,$value) = split /;/,$_; ( $key => $value ); } <$pdfh> };
     close $pdfh;
     return $properdata;
 }
