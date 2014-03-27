0b1a974bd9efe3945759f79d8d76dd21a84d459e
François Gannaz <francois.gannaz@silecs.info>
2014-03-27 13:49:36 +0100

    Simplify module

--- Mirabel.pm
+++ Mirabel.pm
@@ -1,6 +1,8 @@
 package Mirabel;
 
 use XML::Simple;
+use utf8;
+use open qw( :encoding(UTF-8) :std );
 
 use strict;
 use Exporter;
@@ -9,46 +11,44 @@ our @ISA = qw( Exporter );
 our $VERSION = 2.0;
 
 our @EXPORT = qw(
-    &get_services
-    &getConfigPath
-);
+  &get_services
+  &getConfigPath
+  );
 
 sub parse_xml {
-	my ($input) = @_;
-	my $xmlsimple = XML::Simple->new( ForceArray => [ 'revue', 'service' ], SuppressEmpty => '');
-	return $xmlsimple->XMLin($input);
+    my ($input) = @_;
+    my $xmlsimple = XML::Simple->new( ForceArray => [ 'revue', 'service' ], SuppressEmpty => '');
+    return $xmlsimple->XMLin($input);
 }
 
 sub get_services {
     my ($biblio, $properdata, $config) = @_;
 
     my $services = [];
-    foreach ( sort {$a <=> $b} keys %{ $biblio->{services}->{service} } ) {
-	my $service = $biblio->{services}->{service}->{$_};
+    foreach ( sort {$a <=> $b} keys(%{ $biblio->{services}->{service} }) ) {
+        my $service = $biblio->{services}->{service}->{$_};
         $service->{id} = $_;
-	if (!defined $properdata->{ $service->{type} }) {
-	    warn "Type de service inconnu : $service->{type}\n";
-	    next;
-	}
-	my $type = $properdata->{ $service->{type}  };
-	if (!defined $config->{ $type }) {
-	    warn "Pas d'action pour le service : $type\n";
-	    next;
-	}
-	my $id = $service->{id};
-	my $todo = $config->{ $type } if $config->{ $type };
-	next unless $todo;
-        push @$services, {
-	    type => $type,
-	    id => $service->{id},
-	    todo => $config->{ $type },
-	    service => $service
-	};
+        if (!defined $properdata->{ $service->{type} }) {
+            warn "Type de service inconnu : $service->{type}\n";
+            next;
+        }
+        my $type = $properdata->{ $service->{type}  };
+        if (defined $config->{ $type }) {
+            push @$services, {
+                type => $type,
+                id => $service->{id},
+                todo => $config->{ $type },
+                service => $service
+              };
+        } else {
+            warn "Pas d'action pour le service : $type\n";
+        }
     }
     return $services;
 }
 
 sub getConfigPath {
+
     # Read the koha-conf.xml and get configuration path
     my $kohaConfFile = $ENV{'KOHA_CONF'};
     die "Environment variables '\$KOHA_CONF' is not set.\n" unless $kohaConfFile;
@@ -58,7 +58,6 @@ sub getConfigPath {
     my $path = $koha_conf->{config}->{mirabel};
     return $path if $path && ref($path) ne 'HASH';
     return 0;
-
 }
 
 1;
