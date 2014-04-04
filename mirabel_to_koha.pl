8b9d4e5fe0e8f13cf7dde4e5b9d396b6701dcf8b
François Gannaz <francois.gannaz@silecs.info>
2014-04-04 16:32:32 +0200

    Fix a bug that duplicated subfields

--- mirabel_to_koha.pl
+++ mirabel_to_koha.pl
@@ -121,7 +121,7 @@ sub reorder_subfields {
     my $field = shift;
 
     my %list = map { $$_[0] => $$_[1] } ($field->subfields());
-    $field->delete_subfield({}); # remove every subfield
+    $field->delete_subfield('match' => qr//); # remove every subfield
 
     foreach my $key (sort (keys(%list))) {
         $field->add_subfields( $key  => $list{$key} );
