c7250826735f521dcbe6ffb8f57017cb15d7bf64
François Gannaz <francois.gannaz@silecs.info>
2014-04-11 19:00:07 +0200

    Modularize

---
+++ MirabelKoha.pm
@@ -0,0 +1,179 @@
+package MirabelKoha;
+
+use strict;
+use utf8;
+use open qw( :encoding(UTF-8) :std );
+
+use Getopt::Long qw(:config auto_help);
+use Pod::Usage;
+
+use C4::Context;
+use C4::Biblio;
+use MARC::File::USMARC;
+
+require Exporter;
+use vars       qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+# set the version for version checking
+our $VERSION     = 1.00;
+
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(
+	&parse_arguments
+	&validate_options
+	&import_services
+);
+
+
+sub parse_arguments {
+	my $argv = shift;
+	my $opts = {
+		'config' => '',
+		'configkoha' => '',
+	};
+
+	Getopt::Long::GetOptionsFromArray (
+		$argv,
+		$opts,
+		'man|manual',
+		'config=s',
+		'configkoha|config-koha=s',
+		'partenaire|p=i',
+		'issn|n=s',
+		'issnl|l=s',
+		'issne|e=s',
+		'type|t=s',
+		'acces|a=s',
+		'all',
+		'paslacunaire|pas-lacunaire',
+		'passelection|pas-selection',
+		'revue=s',
+		'ressource|r=s',
+		'mesressources',
+		'simulation|dry-run|dryrun',
+	) or die("Erreur : paramètres non valides.\n");
+
+	if (defined $opts->{paslacunaire}) {
+		$opts->{lacunaire} = not $opts->{paslacunaire};
+	}
+	if (defined $opts->{passelection}) {
+		$opts->{selection} = not $opts->{passelection};
+	}
+
+	# Print help thanks to Pod::Usage
+	pod2usage({-verbose => 2, -utf8 => 1, -noperldoc => 1}) if $opts->{man};
+
+	return $opts;
+}
+
+sub validate_options {
+	my $opts = shift;
+
+	if ( (grep {defined($opts->{$_})} qw/issn issnl issne/) > 1 ) {
+		pod2usage(-exitval => "NOEXIT", -verbose => 0);
+		die("## ERREUR : issn, issnl, et issne sont incompatibles.\n");
+	}
+	if ( $opts->{all} and (grep {defined($opts->{$_})}qw/partenaire issn issnl issne type acces lacunaire selection ressource/) ) {
+		pod2usage(-exitval => "NOEXIT", -verbose => 0);
+		die("## ERREUR : --all est incompatible avec d'autres options.\n");
+	}
+}
+
+sub webservice_parameters {
+	my $opts = shift;
+	my $url_args = {};
+	if ($opts->{all}) {
+		$url_args->{all} = undef;
+	} else {
+		foreach (qw/partenaire issn issnl issne type acces selection revue ressource mesressources lacunaire selection/) {
+			$url_args->{$_} = $opts->{$_} if $opts->{$_};
+		}
+	}
+	return $url_args;
+}
+
+sub import_services {
+    my ($biblio, $services, $record) = @_;
+    foreach my $s ( @$services ) {
+
+        #delete_same( $record, $todo, $service);
+
+        my $newfield = createField( $s->{todo}, $s->{service} );
+        reorder_subfields( $newfield );
+
+        my $exists = 0;
+        foreach my $field ( $record->field( $s->{todo}{field} ) ) {
+            my $f3 = $field->subfield('3');
+            if ( $f3 && $s->{id} eq $f3 ) {
+                $exists = 1;
+                $field->replace_with($newfield);
+                print "    field " . $s->{todo}{field} . " updated\n";
+            }
+        }
+        unless ( $exists ) {
+            $record->insert_fields_ordered( $newfield );
+            print "    field " . $s->{todo}{field} . " created\n";
+        }
+        print $newfield->as_formatted ."\n";
+    }
+    my $fmk = GetFrameworkCode( $biblio->{idpartenairerevue} );
+    return ModBiblioMarc( $record, $biblio->{idpartenairerevue}, $fmk );
+}
+
+sub reorder_subfields {
+    my $field = shift;
+
+    my %list = map { $$_[0] => $$_[1] } ($field->subfields());
+    $field->delete_subfield('match' => qr//); # remove every subfield
+
+    foreach my $key (sort (keys(%list))) {
+        $field->add_subfields( $key  => $list{$key} );
+    }
+}
+
+sub delete_same {
+    my $record = shift;
+    my $todo = shift;
+    my $service = shift;
+
+    #foreach my $field ( $record->field( $todo->{field} ) ) {
+    #    my $serv = $field->subfield('b');
+    #    my $acces = $field->subfield('c');
+    #    if ( $serv eq $service->{'nom'} && $acces eq $service->{'acces'} ) {
+    #        print "    => ( x ) Champ supprimé: " . $todo->{field} . "\n";
+    #        $record->delete_field( $field );
+    #    }
+    #}
+
+    foreach my $field ( $record->field( $todo->{field} ) ) {
+        my $id = $field->subfield('3');
+    }
+}
+
+sub createField {
+    my $todo = shift;
+    my $service = shift;
+
+    my $field;
+    my $fieldcreated = 0;
+    foreach my $key ( keys %$todo ) {
+        my $value = build_service_value($todo->{$key}, $service);
+        next unless $value;
+
+        if ( $key ne "field" ) {
+            if ( $fieldcreated ) {
+                $field->add_subfields( $key => $value );
+            } else {
+                #print "    => ( + ) Champ créé: " . $todo->{field} . "\n";
+                $field = MARC::Field->new( $todo->{field},'','', $key => $value );
+                $fieldcreated = 1;
+            }
+        }
+    }
+
+    return $field;
+}
+
+
+1;
+
--- mirabel_to_koha.pl
+++ mirabel_to_koha.pl
@@ -4,73 +4,29 @@ use warnings;
 use utf8;
 use open qw/ :std :utf8 /;
 
-use Getopt::Long qw(:config auto_help);
-use Pod::Usage;
 use C4::Context;
-use XML::Simple;
 use C4::Biblio;
 use MARC::File::USMARC;
-use YAML;
 
 use FindBin;
 use lib "$FindBin::Bin";
+use MirabelKoha;
 use Mirabel;
 
 # remove "experimental" warning in Perl >= 5.18
 no if $] >= 5.017011, warnings => 'experimental::smartmatch';
 
-my %opts = (
-    'config' => '',
-    'configkoha' => '',
-);
-
-GetOptions (
-    \%opts,
-    'man|manual',
-    'config=s',
-    'configkoha|config-koha=s',
-    'partenaire|p=i',
-    'issn|n=s',
-    'issnl|l=s',
-    'issne|e=s',
-    'type|t=s',
-    'acces|a=s',
-    'all',
-    'paslacunaire|pas-lacunaire',
-    'passelection|pas-selection',
-    'revue=s',
-    'ressource|r=s',
-    'mesressources',
-    'simulation|dry-run|dryrun',
-);
-$opts{lacunaire} = !$opts{paslacunaire} if defined $opts{paslacunaire} ;
-$opts{selection} = !$opts{passelection} if defined $opts{passelection};
-
-# Print help thanks to Pod::Usage
-pod2usage({-verbose => 2, -utf8 => 1, -noperldoc => 1}) if $opts{man};
+my %opts = %{parse_arguments(\@ARGV)};
 
 # Load configuration files.
-Mirabel::init(@opts{qw/configkoha config/});
+Mirabel::init($opts{configkoha}, $opts{config});
 my $properdata = Mirabel::read_data_config();
 my $config = Mirabel::read_service_config();
 
-if ( (grep {defined} @opts{qw/issn issnl issne/}) > 1 ) {
-    pod2usage(-verbose => 0, -message => "## ERREUR : issn, issnl, et issne sont incompatibles.\n");
-}
-if ( $opts{all} and (grep {defined} @opts{qw/partenaire issn issnl issne type acces lacunaire selection ressource/}) ) {
-    pod2usage(-verbose => 0, -message => "## ERREUR : --all est incompatible avec d'autres options.\n");
-}
+validate_options(\%opts);
 
-my %url_args = ();
-if ($opts{all}) {
-    $url_args{all} = undef;
-} else {
-    foreach (qw/partenaire issn issnl issne type acces selection revue ressource mesressources lacunaire selection/) {
-        $url_args{$_} = $opts{$_} if $opts{$_};
-    }
-}
 
-my $data = Mirabel::query_webservice($config->{base_url}, \%url_args);
+my $data = Mirabel::query_webservice($config->{base_url}, webservice_parameters(\%opts));
 
 $| = 1;
 foreach my $biblio ( @{ $data->{revue} } ) {
@@ -94,88 +50,6 @@ foreach my $biblio ( @{ $data->{revue} } ) {
 ##################################################################################
 ###                            SUB                                             ###
 ##################################################################################
-
-sub import_services {
-    my ($biblio, $services, $record) = @_;
-    foreach my $s ( @$services ) {
-
-        #delete_same( $record, $todo, $service);
-
-        my $newfield = createField( $s->{todo}, $s->{service} );
-        reorder_subfields( $newfield );
-
-        my $exists = 0;
-        foreach my $field ( $record->field( $s->{todo}{field} ) ) {
-            my $f3 = $field->subfield('3');
-            if ( $f3 && $s->{id} eq $f3 ) {
-                $exists = 1;
-                $field->replace_with($newfield);
-                print "    field " . $s->{todo}{field} . " updated\n";
-            }
-        }
-        unless ( $exists ) {
-            $record->insert_fields_ordered( $newfield );
-            print "    field " . $s->{todo}{field} . " created\n";
-        }
-        print $newfield->as_formatted ."\n";
-    }
-    my $fmk = GetFrameworkCode( $biblio->{idpartenairerevue} );
-    return ModBiblioMarc( $record, $biblio->{idpartenairerevue}, $fmk );
-}
-
-sub reorder_subfields {
-    my $field = shift;
-
-    my %list = map { $$_[0] => $$_[1] } ($field->subfields());
-    $field->delete_subfield('match' => qr//); # remove every subfield
-
-    foreach my $key (sort (keys(%list))) {
-        $field->add_subfields( $key  => $list{$key} );
-    }
-}
-
-sub delete_same {
-    my $record = shift;
-    my $todo = shift;
-    my $service = shift;
-
-    #foreach my $field ( $record->field( $todo->{field} ) ) {
-    #    my $serv = $field->subfield('b');
-    #    my $acces = $field->subfield('c');
-    #    if ( $serv eq $service->{'nom'} && $acces eq $service->{'acces'} ) {
-    #        print "    => ( x ) Champ supprimé: " . $todo->{field} . "\n";
-    #        $record->delete_field( $field );
-    #    }
-    #}
-
-    foreach my $field ( $record->field( $todo->{field} ) ) {
-        my $id = $field->subfield('3');
-    }
-}
-
-sub createField {
-    my $todo = shift;
-    my $service = shift;
-
-    my $field;
-    my $fieldcreated = 0;
-    foreach my $key ( keys %$todo ) {
-        my $value = build_service_value($todo->{$key}, $service);
-        next unless $value;
-
-        if ( $key ne "field" ) {
-            if ( $fieldcreated ) {
-                $field->add_subfields( $key => $value );
-            } else {
-                #print "    => ( + ) Champ créé: " . $todo->{field} . "\n";
-                $field = MARC::Field->new( $todo->{field},'','', $key => $value );
-                $fieldcreated = 1;
-            }
-        }
-    }
-
-    return $field;
-}
 
 __END__
 
